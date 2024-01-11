a8dbf09c7470508b0780fa427e6edbefeb8c884b
Mauricio Massayuki <mauricio.soares@sptech.school>
2024-01-11 17:17:29 -0300

    feat: added react-router-dom to project

--- src/App.js
+++ src/App.js
@@ -5,44 +5,36 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import UserPage from "./pages/UserPage";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
 import logo from "./assets/tddio.png";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   const { t } = useTranslation();
-  const [path, setPath] = useState(window.location.pathname);
-
-  const onClickLink = (event) => {
-    event.preventDefault();
-    const newPath = event.currentTarget.attributes.href.value;
-    window.history.pushState({}, "", newPath);
-    setPath(newPath);
-  };
 
   return (
-    <>
+    <BrowserRouter>
       <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
         <div className="container">
-          <a className="navbar-brand" href="/" title="Home" onClick={onClickLink}>
+          <Link className="navbar-brand" to="/" title="Home">
             <img src={logo} alt="tdd.io logo" width="60" />
             tdd.io
-          </a>
+          </Link>
           <ul className="navbar-nav">
-            <a className="nav-link" href="/signup" title="Sign Up" onClick={onClickLink}>{t("signup")}</a>
-            <a className="nav-link" href="/login" title="Login" onClick={onClickLink}>{t("login")}</a>
+            <Link className="nav-link" to="/signup" title="Sign Up">{t("signup")}</Link>
+            <Link className="nav-link" to="/login" title="Login">{t("login")}</Link>
           </ul>
         </div>
       </nav>
       <div className="container">
-        {window.location.pathname === "/" && <HomePage />}
-        {window.location.pathname === "/signup" && <SignupPage />}
-        {window.location.pathname === "/login" && <LoginPage />}
-        {window.location.pathname.startsWith("/user/") && <UserPage />}
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signup" component={SignupPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/user/:id" component={UserPage} />
         <div className="language-selector-container">
           <LanguageSelector />
         </div>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
